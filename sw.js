4a4eda074dbf8740230b2c9a48d506def50738fe
insterion <insterion@gmail.com>
2026-01-29 14:15:24 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+// Това е нашият "служебен работник"
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
